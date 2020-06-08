51c65c8e21afb319636bbee6ceb69867f5364258
Tera Gilbert <52169004+Tera1971@users.noreply.github.com>
2020-06-08 12:01:58 -0400

    test

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -15,7 +15,48 @@ class App extends React.Component {
     .get('https://api.github.com/users/Tera1971')
     .then(res=>{
       console.log(res.data)
+      axios
+      .get(res.data.followers_url)
+      .then(res=>{
+        console.log(res.data)
+        this.setState({
+          user: res.data,
+          followers: res.data
+        })
+      })
     })
+  }
+  render(){
+    return(
+      <div className = 'first-div'>
+        <h1>Github Usercard</h1>
+        <Card
+         userImg = {this.state.user.avatar_url}
+         name = {this.state.user.name}
+         username = {this.state.user.login}
+         profile = {this.state.user.url}
+         followers = {this.state.user.followers}
+         following = {this.state.user.following}
+         bio = {this.state.user.bio}
+         />
+         <h2>My Followers</h2>
+         {this.state.followers.map(user=>{
+           return
+           <Card
+           userImg = {user.avatar_url}
+           name = {user.name}
+           userName = {user.login}
+           profile = {user.url}
+           followers = {user.followers}
+           following = {user.following}
+           bio = {user.bio}
+          />
+
+          
+        })
+        }
+      </div>
+    )
   }
 }
 
