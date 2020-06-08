7128da0e2ceddaaaf09b5bbc643b5365aecd45b2
Tera Gilbert <52169004+Tera1971@users.noreply.github.com>
2020-06-08 13:16:44 -0400

    cleaned up

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,62 +1,57 @@
-import React, { Component} from 'react';
-import axios from 'axios';
-import './App.css';
-import Card from './components/Card';
+import React, { Component } from "React";
+import axios from "axios";
+import "./App.css";
+import Card from "../components/Card";
 
 class App extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
-    user: '',
-    followers: []
+      user: "",
+      followers: [],
     };
   }
-  componentDidMount(){
-    axios
-    .get('https://api.github.com/users/Tera1971')
-    .then(res=>{
-      console.log(res.data)
-      axios
-      .get(res.data.followers_url)
-      .then(res=>{
-        console.log(res.data)
+  componentDidMount() {
+    axios.get("https://api.github.com/users/Tera1971").then((res) => {
+      console.log(res.data);
+      axios.get(res.data.followers_url).then((res) => {
+        console.log(res.data);
         this.setState({
           user: res.data,
-          followers: res.data
-        })
-      })
-    })
+          followers: res.data,
+        });
+      });
+    });
   }
-  render(){
-    return(
-      <div className = 'first-div'>
+  render() {
+    return (
+      <div className="first-div">
         <h1>Github Usercard</h1>
         <Card
-         userImg = {this.state.user.avatar_url}
-         name = {this.state.user.name}
-         username = {this.state.user.login}
-         profile = {this.state.user.url}
-         followers = {this.state.user.followers}
-         following = {this.state.user.following}
-         bio = {this.state.user.bio}
-         />
-         <h2>My Followers</h2>
-         {this.state.followers.map(user=>{
-           return <Card
-           userImg = {user.avatar_url}
-           name = {user.name}
-           userName = {user.login}
-           profile = {user.url}
-           followers = {user.followers}
-           following = {user.following}
-           bio = {user.bio}
-          />
-
-          
-        })
-        }
+          userImg={this.state.user.avatar_url}
+          name={this.state.user.name}
+          username={this.state.user.login}
+          profile={this.state.user.url}
+          followers={this.state.user.followers}
+          following={this.state.user.following}
+          bio={this.state.user.bio}
+        />
+        <h2>My Followers</h2>
+        {this.state.followers.map((user) => {
+          return (
+            <Card
+              userImg={user.avatar_url}
+              name={user.name}
+              userName={user.login}
+              profile={user.url}
+              followers={user.followers}
+              following={user.following}
+              bio={user.bio}
+            />
+          );
+        })}
       </div>
-    )
+    );
   }
 }
 
