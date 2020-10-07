860b2246e943c616661261c67af63d60e05f19ed
Tera Gilbert <52169004+Tera1971@users.noreply.github.com>
2020-10-06 20:11:01 -0400

    fixed issues

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -9,20 +9,45 @@ class App extends React.Component {
     this.state = {
       user: "",
       followers: [],
+      following: []
     };
   }
+  handleChanges = (e) => {
+    this.setState({
+     
+      ...this.state,
+      followers: e.target.value
+
+    });
+  };
+
   componentDidMount() {
-    axios.get("https://api.github.com/users/Tera1971").then((res) => {
+    axios
+    .get("https://api.github.com/users/Tera1971").then((res) => {
       console.log(res.data);
       axios.get(res.data.followers_url).then((res) => {
         console.log(res.data);
-        this.setState({
-          user: res.data,
-          followers: res.data,
-        });
+        this.setState({ followers: res.data.user });
+      
       });
     });
   }
+  componentDidUpdate(prevState, prevProps) {
+    // if statements here equivalent to useEffect dependency array
+    if (prevState.followers !== this.state.followers) {
+      console.log("followers have changed!");
+    }
+  }
+
+  fetchFollowers = () => {
+    console.log("fetching users");
+    axios
+      .get(`https://api.github.com/users/Tera1971"${this.state.followers}/images`)
+      .then((res) => {
+        this.setState({ followers: res.data.message });
+      })
+      .catch((err) => console.log(err));
+  };
   render() {
     return (
       <div className="first-div">
